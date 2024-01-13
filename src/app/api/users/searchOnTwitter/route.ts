0dfae19a6e3eb22c02b9d50b086f0ca1a9cda511
doxylee <dylee063@gmail.com>
2024-01-14 03:47:28 +0900

    fix: Build error

--- src/app/api/users/searchOnTwitter/route.ts
+++ src/app/api/users/searchOnTwitter/route.ts
@@ -6,14 +6,14 @@ import { TwitterApi } from "twitter-api-v2";
 
 export async function GET(request: NextRequest) {
   return NextResponse.json({ error: "Not implemented due to paid API" }, { status: 501 });
-  if (!await verifyJWT()) return NextResponse.json({ error: "Unauthorized" }, { status: 401 });
+  // if (!await verifyJWT()) return NextResponse.json({ error: "Unauthorized" }, { status: 401 });
 
-  const username = request.nextUrl.searchParams.get("username");
-  if(!username) return NextResponse.json({error: "No username provided"}, {status: 400});
+  // const username = request.nextUrl.searchParams.get("username");
+  // if(!username) return NextResponse.json({error: "No username provided"}, {status: 400});
 
-  const client = new TwitterApi(serverSettings.TWITTER_BEARER_TOKEN);
+  // const client = new TwitterApi(serverSettings.TWITTER_BEARER_TOKEN);
 
-  const userRes = await client.v2.userByUsername(username);
+  // const userRes = await client.v2.userByUsername(username);
 
-  return NextResponse.json(userRes.data);
+  // return NextResponse.json(userRes.data);
 }
