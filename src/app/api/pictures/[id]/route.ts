ebd6d672a3614886e94a437ccd5ff1d2a0698647
doxylee <dylee063@gmail.com>
2024-01-14 19:40:52 +0900

    fix: build error

--- src/app/api/pictures/[id]/route.ts
+++ src/app/api/pictures/[id]/route.ts
@@ -1,7 +1,7 @@
 import { pictureRepository } from "@/_backend/repositories/picture";
-import { NextResponse } from "next/server";
+import { NextRequest, NextResponse } from "next/server";
 
-export async function GET({ params }: { params: { id: string } }) {
+export async function GET(request: NextRequest, { params }: { params: { id: string } }) {
   const id = params.id;
   const picture = await pictureRepository.getPictureById(id);
   if (!picture)
