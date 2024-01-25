8c8c32eee928b68b9b4ddcf77db50bb5d4da35fc
doxylee <dylee063@gmail.com>
2024-01-25 19:46:04 +0900

    feat: image metadata in picture page

--- src/app/(navigation)/pictures/[id]/page.tsx
+++ src/app/(navigation)/pictures/[id]/page.tsx
@@ -6,12 +6,31 @@ import CharactersPart from "./CharactersPart";
 import AuthorsPart from "./AuthorsPart";
 import { getPictureById } from "@/_interface/backend/api/pictures";
 import { FetchError } from "@/utils/fetch";
+import { Metadata, ResolvingMetadata } from "next";
 
-export default async function PicturePage({
-  params,
-}: {
+type Props = {
   params: { id: string };
-}) {
+};
+
+export async function generateMetadata(
+  { params }: Props,
+  parent: ResolvingMetadata,
+): Promise<Metadata> {
+  let picture;
+  try {
+    picture = await getPictureById(params.id);
+  } catch (e) {
+    if (e instanceof FetchError && e.status === 404) return {};
+  }
+
+  return {
+    openGraph: {
+      images: [picture!.mdImage],
+    },
+  };
+}
+
+export default async function PicturePage({ params }: Props) {
   let picture;
   try {
     picture = await getPictureById(params.id);
