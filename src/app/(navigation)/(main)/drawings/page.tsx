89d4e733f789b2a1f510c2f9343ae47ddd1d3867
doxylee <dylee063@gmail.com>
2024-01-31 14:13:01 +0900

    fix: wrong title

--- src/app/(navigation)/(main)/drawings/page.tsx
+++ src/app/(navigation)/(main)/drawings/page.tsx
@@ -23,7 +23,7 @@ export default async function IndexPage({
   return (
     <Container maxWidth="xl" sx={{ px: { xs: 1, sm: 2, md: 4 } }}>
       <Typography variant="h2" mt={4}>
-        최신 사진
+        최신 그림
       </Typography>
       <PictureWall
         page={page}
