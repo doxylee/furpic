20317875fe15e2d90acfa039a47a054110d1f134
doxylee <dylee063@gmail.com>
2024-02-01 16:11:56 +0900

    feat: show character bio, designer

--- src/app/(navigation)/characters/[id]/layout.tsx
+++ src/app/(navigation)/characters/[id]/layout.tsx
@@ -1,7 +1,7 @@
 "use server";
 
 import { NotFoundComponent } from "@/components/404";
-import { Avatar, Paper, Typography } from "@mui/material";
+import { Avatar, AvatarGroup, Paper, Tooltip, Typography } from "@mui/material";
 import { Stack, Container } from "@mui/system";
 import React from "react";
 import { CharacterTabs } from "./CharacterTabs";
@@ -28,7 +28,7 @@ export default async function UserLayout({
   }
 
   return (
-    <Container maxWidth="x2l" sx={{pb: 2}}>
+    <Container maxWidth="x2l" sx={{ pb: 2 }}>
       <Paper
         elevation={3}
         sx={{
@@ -53,7 +53,7 @@ export default async function UserLayout({
               m: { xs: 1, sm: 2 },
             }}
           />
-          <Stack mx={{ xs: 1, sm: 2 }} my={1}>
+          <Stack mx={{ xs: 1, sm: 2 }} my={1} spacing={0.5}>
             <Typography variant="h4">
               {character.nameKo || character.nameEn}
             </Typography>
@@ -74,6 +74,31 @@ export default async function UserLayout({
             sx={{ ml: "auto", mr: { xs: 1, sm: 2 }, my: 1 }}
           />
         </Stack>
+        {character.designers.length > 0 && (
+          <Stack direction="row" alignItems="center" spacing={1}>
+            <Typography fontSize={14} color="#aaa">
+              디자이너:
+            </Typography>
+            <Tooltip
+              title={character.designers.map((a) => a.name).join(", ")}
+              placement="bottom"
+            >
+              <AvatarGroup max={4}>
+                {character.designers.map((designer) => (
+                  <Link href={`/users/@${designer.username}`} key={designer.id}>
+                    <Avatar
+                      src={designer.xsImage ?? undefined}
+                      sx={{ cursor: "pointer", height: 20, width: 20 }}
+                    />
+                  </Link>
+                ))}
+              </AvatarGroup>
+            </Tooltip>
+          </Stack>
+        )}
+        <Typography variant="body1" my={{ xs: 1, sm: 2 }}>
+          {character.bio}
+        </Typography>
         <CharacterTabs />
       </Paper>
       {children}
