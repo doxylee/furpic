0170943a0031fab7804fd8ed22990df7231c2f1a
doxylee <dylee063@gmail.com>
2024-02-06 03:07:54 +0900

    debug

--- src/app/(navigation)/characters/[id]/layout.tsx
+++ src/app/(navigation)/characters/[id]/layout.tsx
@@ -1,16 +1,13 @@
 "use server";
 
 import { NotFoundComponent } from "@/components/404";
-import { Avatar, AvatarGroup, Paper, Tooltip, Typography } from "@mui/material";
-import { Stack, Container } from "@mui/system";
+import { Container } from "@mui/system";
 import React from "react";
-import { CharacterTabs } from "./CharacterTabs";
-import Link from "next/link";
-import { CharacterEditButton } from "./CharacterEditButton";
 import { getCharacterById } from "@/_interface/backend/api/characters";
 import { FetchError } from "@/utils/fetch";
 import { PicturePostFab } from "@/components/PicturePostFab";
 import { Metadata, ResolvingMetadata } from "next";
+import { CharacterPanel } from "./CharacterPanel";
 
 export async function generateMetadata(
   { params }: { params: { id: string } },
@@ -72,87 +69,7 @@ export default async function CharacterLayout({
 
   return (
     <Container maxWidth="x2l" sx={{ pb: 2 }}>
-      <Paper
-        elevation={3}
-        sx={{
-          position: { xs: "static", sm: "sticky" },
-          top: 64,
-          zIndex: 100,
-          backgroundColor: "white",
-          mb: 2,
-          mx: -1,
-          p: 2,
-          pb: 0,
-          borderRadius: "0 0 32px 32px",
-        }}
-      >
-        <Stack direction="row" alignItems="center" flexWrap="wrap">
-          <Avatar
-            src={character.smImage ?? undefined}
-            variant="square"
-            sx={{
-              width: { xs: 100, md: 120 },
-              height: { xs: 100, md: 120 },
-              m: { xs: 1, sm: 2 },
-            }}
-          />
-          <Stack mx={{ xs: 1, sm: 2 }} my={1} spacing={0.5}>
-            <Typography variant="h4">
-              {character.nameKo || character.nameEn}
-            </Typography>
-            {character.user && (
-              <Link href={`/users/@${character.user.username}`}>
-                <Typography
-                  fontSize={14}
-                  color="#aaa"
-                  sx={{ cursor: "pointer" }}
-                >
-                  {character.user.name} @{character.user.username}
-                </Typography>
-              </Link>
-            )}
-          </Stack>
-          <CharacterEditButton
-            character={character}
-            sx={{ ml: "auto", mr: { xs: 1, sm: 2 }, my: 1 }}
-          />
-        </Stack>
-        <Stack direction="row" flexWrap="wrap" alignItems="center">
-          {character.species && (
-            <Typography fontSize={14} color="#aaa" mr={2}>
-              종족: {character.species}
-            </Typography>
-          )}
-          {character.designers.length > 0 && (
-            <Stack direction="row" alignItems="center" spacing={1}>
-              <Typography fontSize={14} color="#aaa">
-                디자이너:
-              </Typography>
-              <Tooltip
-                title={character.designers.map((a) => a.name).join(", ")}
-                placement="bottom"
-              >
-                <div>asdf</div>
-                {/* <AvatarGroup max={4}>
-                  {character.designers.map((designer) => (
-                    <Avatar
-                      component={Link}
-                      href={`/users/@${designer.username}`}
-                      key={designer.id}
-                      src={designer.xsImage ?? undefined}
-                      sx={{ cursor: "pointer", height: 20, width: 20 }}
-                    />
-                  ))}
-                </AvatarGroup> */}
-              </Tooltip>
-            </Stack>
-          )}
-        </Stack>
-        <Typography variant="body1" my={{ xs: 1, sm: 2 }}>
-          {character.bio}
-        </Typography>
-        <CharacterTabs />
-      </Paper>
+      <CharacterPanel character={character} />
       {children}
       <PicturePostFab />
     </Container>
