70ebb86060c550408a93b42685fa1d046b81b219
doxylee <dylee063@gmail.com>
2024-05-09 03:27:55 +0900

    fix: Add missing file

---
+++ src/app/(navigation)/(main)/characters/CharacterSearchFilter.tsx
@@ -0,0 +1,60 @@
+"use client";
+
+import { SelectSpecies } from "@/components/SelectSpecies";
+import { useSpecies } from "@/utils/useSpecies";
+import { Box, Button, Paper, SxProps, TextField } from "@mui/material";
+import Grid2 from "@mui/material/Unstable_Grid2/Grid2";
+import { usePathname, useRouter, useSearchParams } from "next/navigation";
+import { useState } from "react";
+
+export function CharacterSearchFilter({ sx }: { sx?: SxProps }) {
+  const searchParams = useSearchParams();
+  const page = parseInt(searchParams.get("page") || "") || undefined;
+
+  const router = useRouter();
+  const pathname = usePathname();
+
+  const [query, setQuery] = useState<string>(searchParams.get("query") || "");
+  const [species, setSpecies] = useState<string[]>(
+    searchParams.get("species")?.split(",") || [],
+  );
+
+  const handleSearch = () => {
+    let parts = [];
+    if (query) parts.push(`query=${query}`);
+    if (species.length) parts.push(`species=${species.join(",")}`);
+    if (page) parts.push(`page=${page}`);
+    router.push(`${pathname}?${parts.join("&")}`);
+  };
+
+  const handleKeyDown: React.KeyboardEventHandler = (e) => {
+    if (e.charCode === 13 || e.key === "Enter") {
+      e.preventDefault();
+      handleSearch();
+    } 
+  };
+
+  return (
+    <Paper sx={{ p: 2, ...sx }}>
+      <Grid2 container spacing={2}>
+        <Grid2 xs={12} md={6}>
+          <TextField
+            label="검색어"
+            fullWidth
+            value={query}
+            onChange={(e) => setQuery(e.target.value)}
+            onKeyDown={handleKeyDown}
+          />
+        </Grid2>
+        <Grid2 xs={12} md={6}>
+          <SelectSpecies value={species} onChange={setSpecies} label="종족" />
+        </Grid2>
+      </Grid2>
+      <Box display="flex" justifyContent="right" mt={2}>
+        <Button variant="contained" sx={{ ml: "auto" }} onClick={handleSearch}>
+          검색
+        </Button>
+      </Box>
+    </Paper>
+  );
+}
---
+++ src/utils/queryUtils.ts
@@ -0,0 +1,21 @@
+export type Param = string | string[] | undefined;
+
+
+export function getFirst(s: string | string[]): string;
+export function getFirst(s: string | string[] | undefined): string | undefined;
+export function getFirst(s: string | string[] | undefined): string | undefined {
+  if (Array.isArray(s)) return s[0];
+  return s;
+}
+
+export function getFirstInt(s: string | string[] | undefined) {
+  if (Array.isArray(s)) return parseInt(s[0]);
+  if (s) return parseInt(s);
+  return undefined;
+}
+
+export function getCommaList(s: string | string[] | undefined) {
+  if (Array.isArray(s)) return s[0].split(",").map((s) => s.trim());
+  if (s) return s.split(",").map((s) => s.trim());
+  return undefined;
+}
